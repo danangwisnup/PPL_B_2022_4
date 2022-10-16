ea0544a130de14d093be2ccbda31ba4ba7307d55
danangwisnup <danangwisnu@students.undip.ac.id>
2022-10-16 17:08:02 +0700

    finish fitur irs

--- resources/views/mahasiswa/irs/modal.blade.php
+++ resources/views/mahasiswa/irs/modal.blade.php
@@ -7,15 +7,22 @@
         <input type="number" class="form-control" id="jumlah_sks" name="jumlah_sks" placeholder="Jumlah SKS" value="{{ $data->sks }}" required>
     </div>
     <div class="col-12">
+        <input class="form-check-input" type="checkbox" id="confirm" name="confirm">
+        <label class="form-check-label" for="confirm">
+            Ulangi scan IRS?
+        </label>
+    </div>
+    <div class="col-12" id="dropzone">
         <label class="form-label">Scan IRS</label>
-        <div class="dropzone">
+        <div class="dropzone" style="height: 160px;">
             <input type="file" class="filepond" id="fileEdit" name="fileEdit" data-allow-reorder="true">
         </div>
+        <div class="text-danger small fst-italic mt-2 mb-0">*Format file [.pdf], pastikan file yang diupload benar.</div>
+        <div class="small fst-italic mt-0">File baru akan menimpa file sebelumya.</div>
     </div>
-    <div class="text-danger small fst-italic mb-0">*Format file [.pdf], pastikan file yang diupload benar.</div>
-    <div class="small fst-italic mt-0">File baru akan menimpa file sebelumya.</div>
     <div class="col-12 text-end">
         <button type="submit" class="btn btn-sm btn-primary mb-0">Edit</button>
+        <button type="button" class="btn btn-sm btn-secondary mb-0" data-bs-dismiss="modal">Batal</button>
     </div>
 </form>
 
@@ -26,6 +33,16 @@
 
 <!-- Turn all file input elements into ponds -->
 <script>
+    $(document).ready(function() {
+        $('#dropzone').hide();
+        $('#confirm').click(function() {
+            if ($(this).is(':checked')) {
+                $('#dropzone').show();
+            } else {
+                $('#dropzone').hide();
+            }
+        });
+    });
     FilePond.registerPlugin(
         FilePondPluginFileValidateType,
         FilePondPluginFileValidateSize
@@ -35,7 +52,7 @@
         maxParallelUploads: 1,
         maxFileSize: "15MB",
         acceptedFileTypes: ['application/pdf'],
-        labelIdle: '<div class="avatar avatar-sm mt-3"><a class="link"><img class="avatar-img" src="{{ asset("assets/images/upload.png") }}" alt=""></a></div><br/><span class="link">Upload File</span><br/><br/>',
+        labelIdle: '<br/><div class="avatar avatar-sm"><a class="link"><img class="avatar-img" src="{{ asset("assets/images/upload.png") }}" alt=""></a></div><br/><span class="link">Upload File</span><br/>',
         stylePanelAspectRatio: 0.2,
     });
 
