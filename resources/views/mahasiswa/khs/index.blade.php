f2810f896c712b2a1a370ea6238124b75f9ec150
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2022-10-17 14:56:04 +0700

    update fiture mahasiswa

--- resources/views/mahasiswa/khs/index.blade.php
+++ resources/views/mahasiswa/khs/index.blade.php
@@ -14,10 +14,8 @@
 
                 @include('layouts.sidebar')
 
-                <!-- Main content START -->
                 <div class="col-md-8 col-lg-6 vstack gap-4">
-                    {{-- Buat apaan nih event alert? --}}
-                    {{-- Jawab: ... --}}
+
                     <!-- Event alert START -->
                     @if ($errors->any())
                     <div class="alert alert-danger alert-dismissible fade show mb-0" role="alert">
@@ -33,7 +31,47 @@
                     <div class="card">
                         <!-- Card header START -->
                         <div class="card-header d-sm-flex text-center align-items-center justify-content-between border-0 pb-0">
+                            <h1 class="card-title h5">Data KHS</h1>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table" id="table_1">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>Semester</th>
+                                            <th>SKS</th>
+                                            <th>IP</th>
+                                            <th>Status</th>
+                                            <th>Scan KHS</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($khs as $item)
+                                        <tr>
+                                            <td>{{$item->semester_aktif}}</td>
+                                            <td>{{$item->sks}}</td>
+                                            <td>{{$item->ip}}</td>
+                                            <td><a href="{{ asset($item->upload_khs) }}" class="btn btn-info btn-sm"><i class="bi bi-eye"></i> Lihat</a></td></td>
+                                            <td>
+                                                <a href="" class="btn btn-success btn-sm" id="buttonModalKHS" data-bs-toggle="modal" data-bs-target="#editKHS" data-attr="{{ route('khs.edit', [$item->semester_aktif, $item->nim]) }}">
+                                                    <i class="bi bi-pencil-square"></i> Edit
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Card START -->
+                    <div class="card">
+                        <!-- Card header START -->
+                        <div class="card-header d-sm-flex text-center align-items-center justify-content-between border-0 pb-0">
                             <h1 class="card-title h5">KHS</h1>
+                            <div class="text-dark small">Harap diisi dengan data yang benar.</div>
                         </div>
                         <div class="card-body">
                             <form class="row g-3" action="" method="POST">
@@ -90,27 +128,14 @@
                                 <!-- Input IP Kumulatif END -->
 
                                 <!-- Dropzone START-->
-                                <!-- Note:
-                                     - Copy dari template, cuma tampilan blm bisa upload beneran 
-                                     - Ini misal pake style buat memperkecil, area uploadnya tetep dalam area itu apa bisa diluar ya?
-                                       Takutnya kalau area uploadnya diperkecil tapi tetep bisa upload di luar area
-                                -->
-                                <style>
-                                    .dropzone {
-                                        height: 190px;
-                                    }
-                                </style>
-
-                                <div>
+                                <div class="col-12">
                                     <label class="form-label">Scan KHS</label>
-                                    <div class="dropzone dropzone-default card shadow-none" data-dropzone='{"maxFiles":1}'>
-                                        <div class="dz-message">
-                                            <i class="bi bi-upload display-4"></i>
-                                            <p>Upload File</p>
-                                        </div>
+                                    <div class="dropzone">
+                                        <input type="file" class="filepond" id="file" name="file" data-allow-reorder="true">
                                     </div>
                                 </div>
                                 <!-- Dropzone END -->
+                                <div class="text-danger small fst-italic">*Format file [.pdf], pastikan file yang diupload benar.</div>
 
                                 <div class="col-12 text-end">
                                     <button type="submit" class="btn btn-sm btn-primary mb-0">Submit</button>
@@ -129,3 +154,65 @@
 </div>
 
 @endsection
+
+@section('script')
+
+@include('sweetalert::alert')
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<script src="{{ asset('assets/js/javascript-ajax.js') }}"></script>
+<script src="{{ asset('assets/js/data-table.js') }}"></script>
+
+<!-- Load FilePond library -->
+<script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
+<script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
+<script src="https://unpkg.com/filepond@4.17.1/dist/filepond.js"></script>
+
+<!-- Turn all file input elements into ponds -->
+<script>
+    FilePond.registerPlugin(
+        FilePondPluginFileValidateType,
+        FilePondPluginFileValidateSize
+    );
+    FilePond.create(document.getElementById('file'), {
+        maxParallelUploads: 1,
+        maxFileSize: "15MB",
+        acceptedFileTypes: ['application/pdf'],
+        labelIdle: '<br/><div class="avatar avatar-xxl"><a class="link"><img class="avatar-img" src="{{ asset("assets/images/upload.png") }}" alt=""></a></div><br/><span class="link">Upload File</span><br/><br><br/>',
+        stylePanelAspectRatio: 0.2,
+    });
+
+    // Send the files to the Controller
+    FilePond.setOptions({
+        server: {
+            url: '/upload',
+            headers: {
+                'X-CSRF-TOKEN': '{{ csrf_token() }}'
+            }
+        }
+    });
+</script>
+
+<script type="text/javascript">
+    // display a modal dosen
+    $(document).on("click", "#buttonModalKHS", function() {
+        event.preventDefault();
+        let href = $(this).attr("data-attr");
+        $.ajax({
+            url: href,
+            // return the result
+            success: function(result) {
+                $("#editKHS").modal("show");
+                $("#showModalKHS").html(result).show();
+            },
+            error: function(jqXHR, testStatus, error) {
+                console.log(error);
+                alert("Page " + href + " cannot open. Error:" + error);
+            },
+        });
+    });
+</script>
+
+@stop
