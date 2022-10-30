e6c618c89b1fdde79f8a0c2c1f39d675d5e8df36
zhuliana <zhuliana@students.undip.ac.id>
2022-10-30 15:53:49 +0700

    update pesan

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -19,7 +19,7 @@
                 <!-- Main navbar START -->
                 <div class="collapse navbar-collapse" id="navbarCollapse">
 
-                    <ul class="navbar-nav navbar-nav-scroll ms-auto">
+                    <!-- <ul class="navbar-nav navbar-nav-scroll ms-auto">
                         <li class="nav-item ms-2">
                             <a class="icon-md btn btn-primary p-0" href="">
                                 <i class="bi bi-chat-left-text-fill fs-6"> </i>
@@ -30,12 +30,43 @@
                                 <i class="bi bi-gear-fill fs-6"> </i>
                             </a>
                         </li>
-                    </ul>
+                    </ul> -->
+
                 </div>
                 <!-- Main navbar END -->
 
                 <!-- Nav right START -->
                 <ul class="nav flex-nowrap align-items-center list-unstyled">
+
+                    <li class="nav-item dropdown ms-2">
+                        <a class="icon-md btn btn-primary p-0" href="#" id="notifDropdown" role="button" data-bs-auto-close="outside" data-bs-display="static" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="badge-notif animation-blink"></span>
+                            <i class="bi bi-chat-left-text-fill fs-6"> </i>
+                        </a>
+                        <div class="dropdown-menu dropdown-animation dropdown-menu-end dropdown-menu-size-md p-0 shadow-lg border-0" aria-labelledby="notifDropdown">
+                            <div class="card">
+                                <div class="card-body p-0">
+                                    <ul class="list-group list-group-flush list-unstyled p-2">
+                                        <!-- Notif item -->
+                                        <li>
+                                            <a href="#" class="list-group-item list-group-item-action rounded d-flex border-0 mb-1 p-3">
+                                                <div class="avatar text-center d-none d-sm-inline-block">
+                                                    <div class="avatar-img rounded-circle bg-success" src="assets/images/avatar/profile_department.jpg"><span class="text-white position-absolute top-50 start-50 translate-middle fw-bold">DP</span></div>
+                                                </div>
+                                                <div class="ms-sm-3">
+                                                    <div class="d-flex">
+                                                        <p class="small mb-2"><b>Department:</b> Silahkan mengisi progress Anda</p>
+                                                        <p class="small ms-3">1hr</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <!-- Notification dropdown END -->
 
                     <li class="nav-item ms-2 dropdown">
                         <a class="icon-md btn btn-primary p-0" href="" id="profileDropdown" role="button" data-bs-auto-close="outside" data-bs-display="static" data-bs-toggle="dropdown" aria-expanded="false">
